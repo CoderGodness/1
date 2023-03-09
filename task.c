6b10e18a525b7629ce20131e727df1b2ba9b8131
сapi <115636784+capibarco@users.noreply.github.com>
2023-03-09 23:25:44 +0700

    Update task.c

--- task.c
+++ task.c
@@ -3,7 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+double matrixСalc(int size)
+{
+   double error = 0.0;
 
+    #pragma acc parallel loop independent collapse(2) vector vector_length(size) gang num_gangs(size) reduction(max:error) \
+        present(matrixOld[0:size*size], matrixNew[0:size*size])
+    for (size_t i = 1; i < size - 1; i++)
+    {
+        for (size_t j = 1; j < size - 1; j++)
+        {
+		matrixNew[i * size + j] = 0.25 * (
+			matrixOld[i * size + j - 1] + 
+			matrixOld[(i - 1) * size + j] + 
+			matrixOld[(i + 1) * size + j] + 
+			matrixOld[i * size + j + 1]) ;
+		error = fmax(error, matrixNew[i * size + j] - matrixOld[i * size + j]);
+        }
+    }
+  return error;
+}
+void matrixSwap()
+{
+    #pragma acc present(matrixOld[0:size*size], matrixNew[0:size*size])
+    {
+    double* temp = matrixOld;
+    matrixOld = matrixNew;
+    matrixNew = temp;
+    }
+}
+	
 int main(int argc, char** argv)
 {
 	int cornerUL = 10;
@@ -23,7 +52,7 @@ int main(int argc, char** argv)
 	const double fraction = 10.0 / (size - 1);
 	double errorNow = 1.0;
 	int iterNow = 0;
-#pragma acc enter data copyin(errorNow) create(matrixOld[0:totalSize], matrixNew[0:totalSize])
+#pragma acc enter data create(matrixOld[0:totalSize], matrixNew[0:totalSize])
 #pragma acc parallel loop 
 	for (int i = 0; i < size; i++)
 	{
@@ -40,28 +69,11 @@ int main(int argc, char** argv)
 
 	while (errorNow > maxError && iterNow < maxIteration)
 	{
-		double errorLayer = 0;
 		iterNow++;
-#pragma acc parallel loop independent collapse(2) vector vector_length(size) gang num_gangs(size)
-		for (int i = 1; i < size - 1; i++)
-		{
-			for (int j = 1; j < size - 1; j++)
-			{
-				matrixNew[i * size + j] = 0.25 * (
-					matrixOld[i * size + j - 1] + 
-					matrixOld[(i - 1) * size + j] + 
-					matrixOld[(i + 1) * size + j] + 
-					matrixOld[i * size + j + 1]) ;
-				errorLayer = fmax(errorLayer, matrixNew[i * size + j] - matrixOld[i * size + j]);
-				errorNow = errorLayer;
-			}
-		}
-		double* temp = matrixOld;
-		matrixOld = matrixNew;
-		matrixNew = temp;
-		//errorNow = errorLayer;
+		errorNow = matrixСalc(size);
+		matrixSwap();
 	}
-#pragma acc update host(matrixOld[0:totalSize], matrixNew[0:totalSize], errorNow)
+#pragma acc update host(matrixOld[0:totalSize], matrixNew[0:totalSize])
 #pragma acc exit data delete(matrixOld[0:totalSize], matrixNew[0:totalSize])
 	printf("iterations = %d, error = %lf", iterNow, errorNow);
 	printf("\n");
